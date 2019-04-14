4606c7716d847884654dba841459ee71934d5190
loic.martel@outlook.com <loic.martel@outlook.com>
2019-04-14 19:11:53 +0200

    Full delete old

--- src/app/propositions.ts
+++
@@ -1,9 +0,0 @@
-export class Proposition {
-    url: string;
-    id: number;
-    user_from: string;
-    picture_1_path: string;
-    picture_2_path: string;
-    choice_1: string;
-    choice_2: string;
-  }
