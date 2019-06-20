11a03b363061bf033517940074e25ad328a7ee2c
loic.martel@forcity.com <loic.martel@forcity.com>
2019-06-20 15:35:08 +0200

    add hammerjs and component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginScreenComponent } from './login-screen/login-screen.component';
+import { SwipeComponentComponent } from './swipe-component/swipe-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginScreenComponent
+    LoginScreenComponent,
+    SwipeComponentComponent
   ],
   imports: [
     BrowserModule,
